84a5194c9d2b1fd9d8bcad72edabf8e01bcebacc
Marcel Skotnicki <skotnickimarcel@gmail.com>
2017-12-20 14:50:01 +0100

    Version 3.1.1 update.

--- sample/src/main/java/com/synerise/sdk/sample/view/client/ClientAccountActivity.java
+++ sample/src/main/java/com/synerise/sdk/sample/view/client/ClientAccountActivity.java
@@ -17,6 +17,7 @@ import com.synerise.sdk.core.listeners.DataActionListener;
 import com.synerise.sdk.core.model.Sex;
 import com.synerise.sdk.core.net.IApiCall;
 import com.synerise.sdk.core.net.IDataApiCall;
+import com.synerise.sdk.core.utils.Lh;
 import com.synerise.sdk.error.ApiError;
 import com.synerise.sdk.sample.R;
 
@@ -28,8 +29,9 @@ public class ClientAccountActivity extends AppCompatActivity {
 
     public static final String TAG = ClientAccountActivity.class.getSimpleName();
 
-    private IDataApiCall<AccountInformation> dataCall;
-    private IApiCall call;
+    private IDataApiCall<AccountInformation> accountInfoCall;
+    private IApiCall updateAccountCall;
+    private IDataApiCall<String> getTokenCall;
 
     public static Intent createIntent(Context context) {
         return new Intent(context, ClientAccountActivity.class);
@@ -53,14 +55,21 @@ public class ClientAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {ClientAccountActivity.this.updateAccount();}
         });
+        Button getTokenButton = findViewById(R.id.client_get_token);
+        getTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClientAccountActivity.this.getToken();
+            }
+        });
     }
 
     // ****************************************************************************************************************************************
 
     private void getAccount() {
-        if (dataCall != null) dataCall.cancel();
-        dataCall = Client.getAccount();
-        dataCall.execute(new DataActionListener<AccountInformation>() {
+        if (accountInfoCall != null) accountInfoCall.cancel();
+        accountInfoCall = Client.getAccount();
+        accountInfoCall.execute(new DataActionListener<AccountInformation>() {
             @Override
             public void onDataAction(AccountInformation accountInformation) {
                 ClientAccountActivity.this.onGetAccountSuccess(accountInformation);
@@ -87,9 +96,9 @@ public class ClientAccountActivity extends AppCompatActivity {
         AccountInformation accountInformation = new AccountInformation();
         accountInformation.setCity("Warsaw").setSex(Sex.MALE).setCompany("Synerise");
 
-        if (call != null) call.cancel();
-        call = Client.updateAccount(accountInformation);
-        call.execute(new ActionListener() {
+        if (updateAccountCall != null) updateAccountCall.cancel();
+        updateAccountCall = Client.updateAccount(accountInformation);
+        updateAccountCall.execute(new ActionListener() {
             @Override
             public void onAction() {ClientAccountActivity.this.onUpdateAccountSuccess();}
         }, new DataActionListener<ApiError>() {
@@ -109,11 +118,38 @@ public class ClientAccountActivity extends AppCompatActivity {
     }
 
     // ****************************************************************************************************************************************
+    public void getToken() {
+        if (getTokenCall != null) getTokenCall.cancel();
+        getTokenCall = Client.getToken();
+        getTokenCall.execute(new DataActionListener<String>() {
+            @Override
+            public void onDataAction(String token) {
+                onGetTokenSuccess(token);
+            }
+        }, new DataActionListener<ApiError>() {
+            @Override
+            public void onDataAction(ApiError apiError) {
+                onGetTokenFailure(apiError);
+            }
+        });
+    }
+
+    private void onGetTokenSuccess(String token) {
+        Toast.makeText(this, R.string.message_success + " " + token, Toast.LENGTH_LONG).show();
+    }
+
+    private void onGetTokenFailure(ApiError apiError) {
+        Lh.d(getClass().getSimpleName(), "apiError= " + apiError.getErrorBody());
+        String errorCategory = apiError.getHttpErrorCategory().toString();
+        Toast.makeText(this, getString(R.string.message_failure) + " " + errorCategory, Toast.LENGTH_LONG).show();
+    }
+    // ****************************************************************************************************************************************
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (call != null) call.cancel();
-        if (dataCall != null) dataCall.cancel();
+        if (updateAccountCall != null) updateAccountCall.cancel();
+        if (accountInfoCall != null) accountInfoCall.cancel();
+        if (getTokenCall != null) getTokenCall.cancel();
     }
 }
